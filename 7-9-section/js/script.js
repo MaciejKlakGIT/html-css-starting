4d996c0aed8850f947c4068447ffd7c3219f1f77
MaciejKlakGIT <klakmaciej00@gmail.com>
2024-05-07 12:29:47 +0200

    141 done

--- 7-9-section/js/script.js
+++ 7-9-section/js/script.js
@@ -1,9 +1,9 @@
 console.log("Hello world!");
 
-const myName = "Maciej Klak";
+const myName = "Jonas Schmedtmann";
 const h1 = document.querySelector(".heading-primary");
-console.log(h1);
 console.log(myName);
+console.log(h1);
 
 // h1.addEventListener("click", function () {
 //   h1.textContent = myName;
@@ -12,13 +12,13 @@ console.log(myName);
 // });
 
 ///////////////////////////////////////////////////////////
-//Set current year
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
-//Make mobile navigation work
+// Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
@@ -55,6 +55,33 @@ allLinks.forEach(function (link) {
       headerEl.classList.toggle("nav-open");
   });
 });
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
